c7a21f217392dc5a5dd7e8af6c06fcf0df7574fd
therealeddy <eddy.developerr@gmail.com>
2023-06-14 21:54:13 -0300

    feat: storybook

---
+++ packages/react/src/styles/theme/default.ts
@@ -0,0 +1,19 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@desystem-ui/tokens'
+
+export const defaultTheme = {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+}
