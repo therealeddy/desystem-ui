30e5720c191555a952d7bb7b488f26e27c8859fa
therealeddy <eddy.developerr@gmail.com>
2023-06-14 23:09:32 -0300

    feat: fonts

--- packages/react/src/styles/global.ts
+++ packages/react/src/styles/global.ts
@@ -1,7 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
-`
+const GlobalStyles = createGlobalStyle``
 
 export default GlobalStyles
